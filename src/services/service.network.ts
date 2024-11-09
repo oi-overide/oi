3b6a25d611af9be98d51c09df9b47e42a582baac
Abhijeet Dash <43144055+AbhijeetDash@users.noreply.github.com>
2024-11-10 01:12:01 +0530

    Update service.network.ts
    Critical: Fixed test error statement.

--- src/services/service.network.ts
+++ src/services/service.network.ts
@@ -60,7 +60,6 @@ class NetworkServiceImpl extends NetworkService {
     }
 
     const platform = activeServiceDetails.platform;
-    throw new Error('No valid model or platform selected.');
 
     // Handle requests based on the selected platform
     switch (platform) {
